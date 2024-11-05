27acd85def67df518b37ad88d523b659e922dea6
ShahzadMalik07 <shahzadmalik425@gmail.com>
2024-11-05 23:42:12 +0530

    alert message added

--- Paytm Frontend/src/components/Transfer.jsx
+++ Paytm Frontend/src/components/Transfer.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
 
@@ -11,10 +11,38 @@ const Transfer = () => {
   const [amount, setamount] = useState(0)
   const [loading, setloading] = useState(false)
   const navigate = useNavigate()
- 
+
+  const [success, setSuccess] = useState(false);
+
+
+  useEffect(() => {
+    
+    if(success){
+    const timer = setTimeout(() => {
+      navigate("/dashboard"); 
+    }, 10000); 
+
+    return () => clearTimeout(timer);
+  }
+  }, [success,navigate]);
 
   return (
     <div className='bg-white h-screen flex justify-center'>
+      {success && <div
+          style={{
+            backgroundColor: "green",
+            color: "white",
+            padding: "10px",
+            textAlign: "center",
+            fontSize: "18px",
+            position: "fixed",
+            top: "20px",
+            width: "100%",
+            zIndex: "1000",
+          }}
+        >
+          Payment Successful! Redirecting to your dashboard...
+        </div> }
       <div className='w-[40%] bg-gray-200 shadow-md rounded-md mt-10 mb-10 flex justify-center items-center'>
         <div className='bg-white w-[45%] px-2 '>
           <div className='text-center font-bold text-2xl mb-12 mt-4'>Send Money</div>
@@ -43,8 +71,9 @@ const Transfer = () => {
                   Authorization: localStorage.getItem("token")
                 }
               })
+              setSuccess(true)
               setloading(false)
-              navigate("/dashboard")
+            
 
             }} className='mt-3 mb-3 px-2 py-1 bg-green-500 text-white rounded-md' disabled={loading}>{ loading ?"Transferring...":"Transfer Money"}</button>
           </div>
